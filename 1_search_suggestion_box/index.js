7e35cdf7587d9f3ce949c23d798d70774a7ce7db
Pavan S Patil <psp3075@gmail.com>
2023-10-09 21:04:50 +0530

    CustomSwitch

--- 1_search_suggestion_box/index.js
+++ 1_search_suggestion_box/index.js
@@ -5,85 +5,8 @@ import React, {
 } from "https://esm.sh/react@18.2.0";
 import ReactDOM from "https://esm.sh/react-dom@18.2.0";
 
-// DUMMY Server
-const FAILURE_COUNT = 10;
-const LATENCY = 200;
-
-function getRandomBool(n) {
-  const threshold = 1000;
-  if (n > threshold) n = threshold;
-  return Math.floor(Math.random() * threshold) % n === 0;
-}
-
-function getSuggestions(text) {
-  var pre = "pre";
-  var post = "post";
-  var results = [];
-  if (getRandomBool(2)) {
-    results.push(pre + text);
-  }
-  if (getRandomBool(2)) {
-    results.push(text);
-  }
-  if (getRandomBool(2)) {
-    results.push(text + post);
-  }
-  if (getRandomBool(4)) {
-    results.push(pre + text + post);
-  }
-  return new Promise((resolve, reject) => {
-    const randomTimeout = Math.random() * LATENCY;
-    setTimeout(() => {
-      if (getRandomBool(FAILURE_COUNT)) {
-        reject();
-      } else {
-        resolve(results);
-      }
-    }, randomTimeout);
-  });
-}
-
 const App = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [SuggestionVisible, setSuggestionVisible] = useState(false);
-  const [listItems, setListItems] = useState([]);
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setSearchQuery(value);
-    fetchSuggestionData(value);
-  };
-
-  const fetchSuggestionData = async (query) => {
-    try {
-      let response = await getSuggestions(query);
-      setListItems(response);
-    } catch (err) {
-      setListItems([]);
-      console.error(err);
-    }
-  };
-  return (
-    <main className="App">
-      <input
-        type="text"
-        id="search"
-        name="search"
-        placeholder="search..."
-        onFocus={() => setSuggestionVisible(true)}
-        onBlur={() => setSuggestionVisible(false)}
-        onChange={handleChange}
-        value={searchQuery}
-      />
-      {SuggestionVisible && (
-        <div id="suggestion-box">
-          {listItems.map((item) => (
-            <li onClick={(e) => setSearchQuery(e)}>{item}</li>
-          ))}
-        </div>
-      )}
-    </main>
-  );
+  return <div>testing</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- 3_switch comp/index.js
+++ 3_switch comp/index.js
@@ -1,94 +1,53 @@
-// DUMMY Server
-const FAILURE_COUNT = 10;
-const LATENCY = 200;
+import React, {
+  useState,
+  useRef,
+  useEffect, Children
+} from "https://esm.sh/react@18.2.0";
+import ReactDOM from "https://esm.sh/react-dom@18.2.0";
 
-function getRandomBool(n) {
-  const threshold = 1000;
-  if (n > threshold) n = threshold;
-  return Math.floor(Math.random() * threshold) % n === 0;
+const CustomSwitch = ({ children,value})=>{
+  const cases=[];
+  const defaults=[];
+Children.forEach(children, (e)=>{
+  if(e.type.name==="CustomCase"){
+    if(typeof e.props.value === "function"){
+      console.log(e.props.value(value))
+      if(e.props.value(value)){
+      cases.push(e)
+      }
+    }else if(value === e.props.value){
+      cases.push(e)
+    }
+  }else if(e.type.name==="DefaultCase"){
+    defaults.push(e)
+  }
+})  
+  if(cases.length>0) {
+    return cases;
+  }
+  else {
+    return defaults;
+  }
 }
 
-function getSuggestions(text) {
-  var pre = "pre";
-  var post = "post";
-  var results = [];
-  if (getRandomBool(2)) {
-    results.push(pre + text);
-  }
-  if (getRandomBool(2)) {
-    results.push(text);
-  }
-  if (getRandomBool(2)) {
-    results.push(text + post);
-  }
-  if (getRandomBool(4)) {
-    results.push(pre + text + post);
-  }
-  return new Promise((resolve, reject) => {
-    const randomTimeout = Math.random() * LATENCY;
-    setTimeout(() => {
-      if (getRandomBool(FAILURE_COUNT)) {
-        reject();
-      } else {
-        resolve(results);
-      }
-    }, randomTimeout);
-  });
-}
+const CustomCase = ({children})=>(<>{children}</>)
 
-(function () {
-  const input = document.getElementById("search");
-  const suggestionBox = document.getElementById("suggestion-box");
+const DefaultCase = ({children})=>(<>{children}</>)
 
-  const onFocus = () => {
-    suggestionBox.style.display = "block";
-  };
+const App = () => {
+  
+  return (
+    <>
+      <CustomSwitch value="10000">
+        <CustomCase value={(e) => e< 10}>
+          <p>hello 10</p>
+        </CustomCase>
+        <CustomCase value="20">hello 20</CustomCase>
+        <CustomCase value="30">hello 30</CustomCase>
+        <DefaultCase>Hello 40</DefaultCase>
+      </CustomSwitch>
+    </>
+  );
+};
 
-  const onBlur = (e) => {
-    if (e.target === input || e.target === suggestionBox) {
-      return;
-    }
-    suggestionBox.style.display = "none";
-  };
-
-  const onChange = (e) => {
-    processData(e.target.value);
-  };
-
-  const processData = async (value) => {
-    suggestionBox.innerHTML = "";
-    if (!value) {
-      return;
-    }
-    try {
-      const response = await getSuggestions(value);
-      if (response.length > 0) {
-        const list = document.createElement("ul");
-        response.forEach((item) => {
-          const listItems = document.createElement("li");
-          listItems.style.cursor = "pointer";
-          listItems.innerText = item;
-          list.appendChild(listItems);
-        });
-        suggestionBox.innerHTML = "";
-        suggestionBox.appendChild(list);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const onClick = (e) => {
-    if (e.target === suggestionBox) {
-      return;
-    }
-    const text = e.target.innerText;
-    input.value = text;
-    input.focus();
-  };
-
-  input.addEventListener("focus", onFocus);
-  window.addEventListener("click", onBlur);
-  input.addEventListener("keyup", onChange);
-  suggestionBox.addEventListener("click", onClick, true);
-})();
+ReactDOM.render(<App />, document.getElementById("root"));
